373771009d59f4576a3fdcb1705b8d0ab7d93997
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-01-28 18:27:17 -0300

    feat: add chiseled stone bricks block

--- frontend/src/Blocks.js
+++ frontend/src/Blocks.js
@@ -11,6 +11,7 @@ import ItemCat from './ItemCategories';
 export default {
     Air: ( new Block('air').setTransparency(true) ),
 
+    // Natural Blocks
     GrassBlock: ( new BlockNatural('grass-block').setModel(blockModelsEnum.Side)),
     Dirt: ( new BlockNatural('dirt') ),
     Grass: ( new BlockX('grass') ),
@@ -23,18 +24,20 @@ export default {
     EmeraldOre: ( new BlockNatural('emerald-ore') ),
     LapisLazuliOre: ( new BlockNatural('lapis-lazuli-ore') ),
     DiamondOre: ( new Block('diamond-ore', ItemCat.NaturalBlocks) ),
+    Leaves: ( new BlockNatural('leaves') ),
+    MossBlock: ( new BlockNatural('moss-block') ),
+    Dandelion: (new BlockX('flower--dandelion') ),
 
+    // Building Blocks
     OakPlanks: ( new BlockBuilding('oak-planks') ),
     Cobblestone: ( new BlockBuilding('cobblestone') ),
     Bricks: ( new BlockBuilding('bricks') ),
     StoneBricks: ( new BlockBuilding('stone-bricks') ),
+    ChiseledStoneBricks: ( new BlockBuilding('chiseled-stone-bricks') ),
     CrackedStoneBricks: ( new BlockBuilding('cracked-stone-bricks') ),
     MossyStoneBricks: ( new BlockBuilding('mossy-stone-bricks') ),
     StoneBricksSlab: ( new BlockSlab('stone-bricks') ),
     StoneBricksWall: ( new BlockWall('stone-bricks') ),
-    Leaves: ( new BlockNatural('leaves') ),
-    MossBlock: ( new BlockNatural('moss-block') ),
-    IronBars: ( new Block('iron-bars').setModel(blockModelsEnum.Bars).setTransparency(true).setStates([BStates.FacingXZ]) ),
     DioriteWall: ( new BlockWall('diorite') ),
-    Dandelion: (new BlockX('flower--dandelion') )
+    IronBars: ( new Block('iron-bars').setModel(blockModelsEnum.Bars).setTransparency(true).setStates([BStates.FacingXZ]) ),
 }
