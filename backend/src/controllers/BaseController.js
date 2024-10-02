ba7169c15047ee9b29500f33c5cc83dc9e78d160
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-10-02 02:19:39 -0300

    [WIP] feat: add handleError to BaseController

--- backend/src/controllers/BaseController.js
+++ backend/src/controllers/BaseController.js
@@ -1,6 +1,10 @@
 require('../utils/typedef');
 
+const { NotFoundError } = require('../errors/NotFoundError');
+const { UnprocessableContentError } = require('../errors/UnprocessableContentError');
+const { UnauthorizedError } = require('../errors/UnauthorizedError');
 const { HttpResponse } = require('../http/HttpResponse');
+const { Logger } = require('../logger/Logger');
 
 class BaseController {
     /**
@@ -19,10 +23,27 @@ class BaseController {
             const { statusCode, body } = response;
             return res.status(statusCode).json(body);
         } catch (error) {
-            response = HttpResponse.serverError();
+            response = await BaseController.handleError(error);
             const { statusCode, body } = response;
             return res.status(statusCode).json(body);
         }
+    }
+
+    static async handleError(error) {
+        let response;
+
+        if (error instanceof UnauthorizedError) {
+            response = HttpResponse.unauthorized(error.message);
+        } else if (error instanceof NotFoundError) {
+            response = HttpResponse.notFound(error.message);
+        } else if (error instanceof UnprocessableContentError) {
+            response = HttpResponse.unprocessable(error.message);
+        } else {
+            Logger.error(error);
+            response = HttpResponse.serverError();
+        }
+
+        return response;
     }
 }
 
