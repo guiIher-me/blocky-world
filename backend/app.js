fb59db0149c92642678f1520dc426fafb43f7430
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-10-02 02:23:09 -0300

    feat: add loggers

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,37 @@
-/* eslint-disable import/order */
+/* eslint-disable import/no-extraneous-dependencies */
 require('./src/db');
-
 require('dotenv').config();
-
-const { FRONTEND_URL, SERVER_PORT } = process.env;
-
+const moment = require('moment-timezone');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { verifyAuthMiddleware } = require('./src/middlewares/verifyAuthMiddleware');
-const { notFoundMiddleware } = require('./src/middlewares/notFoundMiddleware');
 const router = require('./src/routes');
+const { Logger } = require('./src/logger/Logger');
+const { requestLoggerMiddleware } = require('./src/middlewares/logs/requestLoggerMiddleware');
+const { responseLoggerMiddleware } = require('./src/middlewares/logs/responseLoggerMiddleware');
 
 const app = express();
+const { FRONTEND_URL, SERVER_PORT } = process.env;
 
-const cors = require('cors');
+// Datetime Configurations
+moment.tz.setDefault('America/Sao_Paulo');
 
+// Cors Configurations
 const corsOptions = {
     origin: FRONTEND_URL,
     optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 
+// Middlewares Setup
 app.use(cors(corsOptions));
-
 app.use(bodyParser.json());
 app.use(verifyAuthMiddleware);
+app.use(requestLoggerMiddleware);
+app.use(responseLoggerMiddleware);
 app.use(router);
-app.use(notFoundMiddleware);
 
+// Listen
 app.listen(SERVER_PORT, () => {
-    console.log(`Servidor rodando na porta ${SERVER_PORT} !`);
+    Logger.info(`Server ON [PORT:${SERVER_PORT}]`);
 });
