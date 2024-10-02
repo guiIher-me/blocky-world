e1fade839a2701a43123ff31e5f1f751f52ad3eb
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-10-02 02:05:45 -0300

    refactor(healthCheck): change /test to /ping

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const { WorldController } = require('./controllers/world/WorldController');
 const { BaseController } = require('./controllers/BaseController');
 
+// Health Check
+router.get('/ping', async (_, res) => {
+    res.status(200).json({ message: 'pong!' });
+});
+
 // Worlds
 router.post('/worlds', (req, res) => BaseController.handle(req, res, WorldController.create));
 router.get('/worlds', async (req, res) => BaseController.handle(req, res, WorldController.getAll));
 router.get('/worlds/:id', async (req, res) => BaseController.handle(req, res, WorldController.getById));
 router.put('/worlds/:id', async (req, res) => BaseController.handle(req, res, WorldController.update));
 router.delete('/worlds/:id', async (req, res) => BaseController.handle(req, res, WorldController.delete));
-
-// Test
-router.get('/', async (req, res) => {
-    res.status(200).json({ message: "It's Working!" });
-});
 
 module.exports = router;
