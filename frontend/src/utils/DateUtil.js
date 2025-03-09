5d51e66c89f7ddb0d579e5c7b66bbe9ef3e524f6
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2025-03-09 02:42:51 -0300

    refactor: reuse logic implementation

--- frontend/src/utils/DateUtil.js
+++ frontend/src/utils/DateUtil.js
@@ -9,16 +9,16 @@ export default class DateUtil {
         return numberExpirationAt;
     }
 
-    static isExpired(expirationAt) {
-        const numberExpirationAt = DateUtil.assert(expirationAt);
-        const now = new Date();
-        const expirationDate = new Date(numberExpirationAt * 1000);
-        return now > expirationDate;
-    }
-
     static getExpirationDate(expirationAt) {
         const numberExpirationAt = DateUtil.assert(expirationAt);
         const expirationDate = new Date(numberExpirationAt * 1000);
+        
         return expirationDate;
+    }
+
+    static isExpired(expirationAt) {
+        const expirationDate = DateUtil.getExpirationDate(expirationAt);
+        const now = new Date();
+        return now > expirationDate;
     }
 }
