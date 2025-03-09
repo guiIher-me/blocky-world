7e16eb004ba57f5b1731c094a61e9e40c7ba765f
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2025-03-09 03:13:11 -0300

    feat: remove unused http/index

--- frontend/src/http/index.js
+++
@@ -1,7 +0,0 @@
-import BlockyHttp from './BlockyHttp';
-import BlockyHttpAuth from './BlockyHttpAuth';
-
-export default {
-    BlockyHttp: new BlockyHttp(),
-    BlockyHttpAuth: new BlockyHttpAuth()
-}
