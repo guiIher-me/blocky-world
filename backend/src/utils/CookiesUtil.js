594b457832ce80a998506f709d0dcb90c113b9e9
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2025-03-09 00:00:56 -0300

    fix: use miliseconds insted seconds  in expirationAt

--- backend/src/utils/CookiesUtil.js
+++ backend/src/utils/CookiesUtil.js
@@ -2,7 +2,7 @@ class CookiesUtil {
     static setCookie(res, name, value, expirationAt, secure = false) {
         const options = {
             path: '/',
-            expires: expirationAt ? new Date(Number(expirationAt)) : undefined,
+            expires: expirationAt ? new Date(Number(expirationAt * 1000)) : undefined,
             secure,
             httpOnly: true,
             sameSite: 'none',
